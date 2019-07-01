517192e59ac59e297d0799eb86f54c9ddd91b24b
jay521github <jay521mm@126.com>
2019-07-01 11:18:45 +0800

    1

--- OnLine/TMS/question_answer.py
+++ OnLine/TMS/question_answer.py
@@ -1,34 +1,37 @@
 from selenium import webdriver
-import unittest
-class Qa(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get('http://tms.beiwaiguoji.com/login/login')
-        self.driver.maximize_window()
-        self.driver.find_element_by_xpath("//input[@name='loginname']").send_keys('admin')
-        self.driver.find_element_by_xpath("//input[@name='password']").send_keys('bwgj')
-        self.driver.find_element_by_name('validNumber').send_keys(6666)
-        self.driver.find_element_by_xpath("//a[@class='landbtn']").click()
-        self.driver.implicitly_wait(30)
-    def tearDown(self):
-        self.driver.quit()
-    def test(self):
-        js = "var q=document.body.scrollTop=500"
-        self.driver.execute_script(js)
-        self.driver.find_element_by_xpath('//*[@id="sub_0"]/li[22]/a/span').click()
-        js = "var q=document.body.scrollTop=0"
-        self.driver.execute_script(js)
-        self.driver.find_element_by_xpath('//*[@id="queryForm"]/div[2]/div/div[2]/button').click()
-        self.driver.find_element_by_xpath('//*[@id="queryForm"]/div[2]/div/div[2]/ul/li[3]/a/label').click()
-        self.driver.find_element_by_xpath('//*[@id="queryForm"]/button').click()
-        Page_text=self.driver.find_element_by_xpath('//*[@id="bslist"]').text
-        Page_data='没有数据！'
-        file_path = r'C:\Users\Administrator\Desktop\Tms.txt'
-        if Page_data in Page_text:
-            print('没有待处理的问题')
-        else:
-            with open(file_path,'w')as fp:
-                fp.write(Page_text)
+if __name__ == '__main__':
+
+
+
+
+    driver = webdriver.Chrome()
+    driver.get('http://tms.beiwaiguoji.com/login/login')
+    driver.maximize_window()
+    driver.find_element_by_xpath("//input[@name='loginname']").send_keys('admin')
+    driver.find_element_by_xpath("//input[@name='password']").send_keys('bwgj')
+    driver.find_element_by_name('validNumber').send_keys(6666)
+    driver.find_element_by_xpath("//a[@class='landbtn']").click()
+    driver.implicitly_wait(30)
+
+
+
+    js = "var q=document.body.scrollTop=500"
+    driver.execute_script(js)
+    driver.find_element_by_xpath('//*[@id="sub_0"]/li[22]/a/span').click()
+    js = "var q=document.body.scrollTop=0"
+    driver.execute_script(js)
+    driver.find_element_by_xpath('//*[@id="queryForm"]/div[2]/div/div[2]/button').click()
+    driver.find_element_by_xpath('//*[@id="queryForm"]/div[2]/div/div[2]/ul/li[3]/a/label').click()
+    driver.find_element_by_xpath('//*[@id="queryForm"]/button').click()
+    Page_text=driver.find_element_by_xpath('//*[@id="bslist"]').text
+    Page_data='没有数据！'
+    file_path = r'C:\Users\Administrator\Desktop\Tms.txt'
+    if Page_data in Page_text:
+        print('没有待处理的问题')
+    else:
+        with open(file_path,'w')as fp:
+            fp.write(Page_text)
+    driver.quit()
 
 
 
@@ -37,6 +40,3 @@ class Qa(unittest.TestCase):
 
 
 
-
-        if __name__ == '__main__':
-            unittest.main()
