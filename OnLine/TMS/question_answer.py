91560c4368a3a2573fbaea2bcffa201e0bfe6fc1
jay521github <jay521mm@126.com>
2019-08-12 08:55:12 +0800

    修改登陆密码

--- OnLine/TMS/question_answer.py
+++ OnLine/TMS/question_answer.py
@@ -7,7 +7,7 @@ driver = webdriver.Chrome()
 driver.get('http://tms.beiwaiguoji.com/login/login')
 driver.maximize_window()
 driver.find_element_by_xpath("//input[@name='loginname']").send_keys('admin')
-driver.find_element_by_xpath("//input[@name='password']").send_keys('bwgj')
+driver.find_element_by_xpath("//input[@name='password']").send_keys('bwgj@2019')
 driver.find_element_by_name('validNumber').send_keys(6666)
 driver.find_element_by_xpath("//a[@class='landbtn']").click()
 driver.implicitly_wait(30)
@@ -25,8 +25,6 @@ Page_data='没有数据！'
 file_path = r'C:\Users\Administrator\Desktop\Tms.txt'
 if Page_data in Page_text:
     print('没有待处理的问题')
-
-
 else:
     with open(file_path,'w')as fp:
         fp.write(Page_text)
